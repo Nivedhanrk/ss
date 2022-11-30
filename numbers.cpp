7ce1f724a440130787a21cab640aa0b4218ae264
Nivedhanrk <119484274+Nivedhanrk@users.noreply.github.com>
2022-11-30 14:58:32 +0530

    sharmila

---
+++ numbers.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	
+int num[] = {2,8,7,6,0,9};
+
+	            printf("\n %d",num[0]);
+	            printf("\n %d",num[1]);
+	            printf("\n %d",num[2]);
+	            printf("\n %d",num[3]);
+	            printf("\n %d",num[4]);
+	            printf("\n %d",num[5]);
+	    
+		
+return 0;		        
+}
